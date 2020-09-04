e81c0555441bfd1561a84b46c019d4f7dc3edb01
Federico Medini <medo.federico@gmail.com>
2020-09-04 14:33:17 +0200

    login final version

--- public/federico/js/login.js
+++ public/federico/js/login.js
@@ -73,66 +73,12 @@ form.onsubmit = () => {
         return true;
     } else if (isUsernameValid()) {
         console.log('Field "PASSWORD" is wrong! \n Submit aborted');
-        alert('Password must be at least 8 characters long and must contain: \n - at least one lowercase character \n - at least one upper case character \n - at least one number');
         return false;
     } else if (isPasswordValid()) {
         console.log('Field "USERNAME" is empty! \n Submit aborted');
-        alert('You must enter a  username');
         return false;
     } else {
         console.log('Both "USERNAME" and "PASSWORD" are wrong! \n Submit aborted');
-        alert('You must enter valid username and password');
         return false;
     }
 };
-
-/*
-    if (psw.value.length == 0) {
-        console.log('Field "PASSWORD" is empty! \n Submit aborted');
-        alert('You must enter a  password');
-        psw.classList.add('borderRed');
-        pswFlag = false;
-    } else if (psw.value.length < 8) {
-        console.log('Field "PASSWORD" is wrong! \n Submit aborted');
-        alert('Password must be at least 8 characters long and must contain: \n - at least one lowercase character \n - at least one upper case character \n - at least one number');
-        psw.classList.add('borderRed');
-        pswFlag = false;
-    } else {
-        let cond1 = false;
-        let cond2 = false;
-        let cond3 = false;
-        let password = psw.value;
-        let i = 0;
-
-        while (i <= password.length) {
-            character = password.charAt(i);
-            if (!isNaN(character * 1)) {
-                cond1 = true;
-            } else {
-                if (character == character.toUpperCase()) {
-                    cond2 = true;
-                }
-                if (character == character.toLowerCase()) {
-                    cond3 = true;
-                }
-            }
-            i++;
-        }
-        if (!(cond1 && cond2 && cond3)) {
-            console.log('Field PASSWORD is wrong! \n Submit aborted');
-            alert('Password must be at least 8 characters long and must contain: \n - at least one lowercase character \n - at least one upper case character \n - at least one number');
-            psw.classList.add('borderRed');
-            pswFlag = false;
-        }
-    }
-    if (usrFlag && pswFlag) {
-        if (psw.classList.contains('borderRed')) {
-            psw.classList.remove('borderRed');
-        }
-        alert('Autentication successfully completed');
-        return true;
-    } else {
-        return false;
-    }
-};
-*/
